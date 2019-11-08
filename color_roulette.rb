7f63e68042f436f2076316b56efbeacbca89aadc
Kenneth Chuck <krchuck@outlook.com>
2019-11-08 15:26:37 -0700

    initial commit

--- color_roulette.rb
+++ color_roulette.rb
@@ -1,33 +1,61 @@
-require_relative 'main'
-require_relative 'heads_tails'
-require_relative 'player'
-require_relative 'wallet'
+require "pry"
+# require_relative 'main'
+# # require_relative 'heads_tails'
+# require_relative 'player'
+# require_relative 'wallet'
 
 class Color_roulette
+  # attr_accessor :colorbets
     def initialize 
-      colorbets = []
+      # @colorbets = ["red", "black"]
+      ritmenu
     end 
 
     def ritmenu 
+      @colorbets = ["red", "black"]
       puts "Welcome to Roulette! You can bet on red or black. Please make a selection."
-      puts "1) Red"
-      puts "2) Black"
-      ritcolor = gets.to_i
-      case ritcolor
-      when 1
-        @colorbets.push(color)
-      when 2
-        @colorbets.push(color)
-        binding.pry
-      else
-        puts "Invalid selection"
-        ritmenu
+      ritcolor = gets.chomp
+      ritbetting
+      case ritcolor 
+      when "red"
+        if ritcolor == @colorbets.sample 
+        puts "YOU WIN"
+        else 
+          puts "you lose"
+        end
+        ritplayagain
+      when "black"
+         if @colorbets.sample == ritcolor 
+        puts "YOU WIN"
+        else 
+          puts "you lose"
+        end
+        ritplayagain
+      else 
+          puts "invalid option"
+          ritplayagain
       end
-      puts "Place your bet!"
-      
+
     end 
 
-    def ritrand
+    def ritbetting
+      puts "How much would you like to bet?"
+      ritbetamt = gets.to_i
+    end
+
+    def ritplayagain
+      puts "Would you like to play again? Y/N"
+        ritpickagain = gets.strip
+      case ritpickagain
+       when "Y"
+          ritmenu
+       when "N"
+          exit
+       else
+        "Invalid selection, select Y/N"
+        ritplayagain
+       end
 
     end 
 end 
+Color_roulette.new 
