9e5fc27bc5d13568dbd9cb9bff43ac454df225b0
Spencer Thomas <spencerthomas@Spencers-MacBook-Air.local>
2019-11-08 13:49:02 -0700

    heads and tails logic

--- heads_tails.rb
+++ heads_tails.rb
@@ -5,14 +5,27 @@ require_relative 'wallet'
 
 class Heads_tails
   def initialize 
+    puts "select heads or tails"
+    @coin = ["heads", "tails"
+  ]
+    @bets = [
+      { name: "min", amount: 10 },
+      { name: "max", amount: 25 }
+    ]
+    users_guess = gets.chomp
+    if @coin.sample == users_guess
+      puts "YOU WIN!"
+      # add method from wallet
+    else
+      puts "you lose..."
+      # lose method from wallet
+    end
 
   end 
 
-  def heads_tails_menu 
-    #menu to play game 
-  end 
-
-  def heads_tails_rand
+  def betting
+  
+  end
 
   end 
 end 
