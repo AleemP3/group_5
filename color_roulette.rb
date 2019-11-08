e33407b178f171bf9bbbcec47f346e60c89bf3ca
Kenneth Chuck <krchuck@outlook.com>
2019-11-08 13:49:50 -0700

    initial commit

--- color_roulette.rb
+++ color_roulette.rb
@@ -1,3 +1,5 @@
+binding.pry
+
 require_relative 'color_roulette'
 require_relative 'heads_tails'
 require_relative 'player'
@@ -5,11 +7,24 @@ require_relative 'wallet'
 
 class Color_roulette
     def initialize 
-
+      colorbets = []
     end 
 
     def ritmenu 
-      #menu to play game 
+      puts "Welcome to Roulette! You can bet on red or black. Please make a selection."
+      puts "1) Red"
+      puts "2) Black"
+      color = gets.to_i
+      case ritbet
+      when 1
+        @colorbets.push(color)
+      when 2
+        @colorbets.push(color)
+        binding.pry
+      else
+        puts "Invalid selection"
+        ritmenu
+      end
     end 
 
     def ritrand
